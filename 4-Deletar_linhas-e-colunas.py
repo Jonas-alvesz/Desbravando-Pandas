c353e875db4f3b10f2b3a39e706c0433a8bfea6f
Jonas Alves <165534987+Jonas-alvesz@users.noreply.github.com>
2024-09-08 19:48:24 -0300

    Update 4-Deletar_linhas-e-colunas.py

--- 4-Deletar_linhas-e-colunas.py
+++ 4-Deletar_linhas-e-colunas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-dados=pd.read_excel(r'C:\Users\jonas\Desktop\Tratamento de Dados\Arquivos Excel\Deletar_Linhas_Colunas.xlsx')
+dados=pd.read_excel('./Arquivos Excel/Deletar_Linhas_Colunas.xlsx')
 
 print('\nExibindo tudo do arquivo excel(xlsr):')
 print(dados)
